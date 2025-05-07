7a6867ce868c90b6d3a6d769ca36b612acb7dd09
595554963github <595554963@qq.com>
2025-05-07 22:24:59 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace QuickBMSBatchExtractor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBmsScript = new System.Windows.Forms.Label();
+            this.txtBmsScript = new System.Windows.Forms.TextBox();
+            this.btnSelectBmsScript = new System.Windows.Forms.Button();
+            this.lblInputFolder = new System.Windows.Forms.Label();
+            this.txtInputFolder = new System.Windows.Forms.TextBox();
+            this.btnSelectInputFolder = new System.Windows.Forms.Button();
+            this.lblAvailableFormats = new System.Windows.Forms.Label();
+            this.btnSelectFormat = new System.Windows.Forms.Button();
+            this.lblSelectedFiles = new System.Windows.Forms.Label();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.rtbFileNameInfo = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblBmsScript
+            // 
+            this.lblBmsScript.AutoSize = true;
+            this.lblBmsScript.Location = new System.Drawing.Point(14, 20);
+            this.lblBmsScript.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBmsScript.Name = "lblBmsScript";
+            this.lblBmsScript.Size = new System.Drawing.Size(86, 17);
+            this.lblBmsScript.TabIndex = 0;
+            this.lblBmsScript.Text = "选择BMS脚本:";
+            // 
+            // txtBmsScript
+            // 
+            this.txtBmsScript.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBmsScript.Location = new System.Drawing.Point(104, 16);
+            this.txtBmsScript.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBmsScript.Name = "txtBmsScript";
+            this.txtBmsScript.Size = new System.Drawing.Size(645, 23);
+            this.txtBmsScript.TabIndex = 1;
+            // 
+            // btnSelectBmsScript
+            // 
+            this.btnSelectBmsScript.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectBmsScript.Location = new System.Drawing.Point(756, 13);
+            this.btnSelectBmsScript.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelectBmsScript.Name = "btnSelectBmsScript";
+            this.btnSelectBmsScript.Size = new System.Drawing.Size(88, 30);
+            this.btnSelectBmsScript.TabIndex = 2;
+            this.btnSelectBmsScript.Text = "选择脚本";
+            this.btnSelectBmsScript.UseVisualStyleBackColor = true;
+            this.btnSelectBmsScript.Click += new System.EventHandler(this.btnSelectBmsScript_Click);
+            // 
+            // lblInputFolder
+            // 
+            this.lblInputFolder.AutoSize = true;
+            this.lblInputFolder.Location = new System.Drawing.Point(14, 54);
+            this.lblInputFolder.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInputFolder.Name = "lblInputFolder";
+            this.lblInputFolder.Size = new System.Drawing.Size(71, 17);
+            this.lblInputFolder.TabIndex = 3;
+            this.lblInputFolder.Text = "选择文件夹:";
+            // 
+            // txtInputFolder
+            // 
+            this.txtInputFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInputFolder.Location = new System.Drawing.Point(104, 50);
+            this.txtInputFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtInputFolder.Name = "txtInputFolder";
+            this.txtInputFolder.Size = new System.Drawing.Size(645, 23);
+            this.txtInputFolder.TabIndex = 4;
+            // 
+            // btnSelectInputFolder
+            // 
+            this.btnSelectInputFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectInputFolder.Location = new System.Drawing.Point(756, 47);
+            this.btnSelectInputFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelectInputFolder.Name = "btnSelectInputFolder";
+            this.btnSelectInputFolder.Size = new System.Drawing.Size(88, 30);
+            this.btnSelectInputFolder.TabIndex = 5;
+            this.btnSelectInputFolder.Text = "选择路径";
+            this.btnSelectInputFolder.UseVisualStyleBackColor = true;
+            this.btnSelectInputFolder.Click += new System.EventHandler(this.btnSelectInputFolder_Click);
+            // 
+            // lblAvailableFormats
+            // 
+            this.lblAvailableFormats.AutoSize = true;
+            this.lblAvailableFormats.Location = new System.Drawing.Point(14, 88);
+            this.lblAvailableFormats.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAvailableFormats.Name = "lblAvailableFormats";
+            this.lblAvailableFormats.Size = new System.Drawing.Size(0, 17);
+            this.lblAvailableFormats.TabIndex = 6;
+            // 
+            // btnSelectFormat
+            // 
+            this.btnSelectFormat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectFormat.Location = new System.Drawing.Point(756, 81);
+            this.btnSelectFormat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelectFormat.Name = "btnSelectFormat";
+            this.btnSelectFormat.Size = new System.Drawing.Size(88, 30);
+            this.btnSelectFormat.TabIndex = 7;
+            this.btnSelectFormat.Text = "选择格式";
+            this.btnSelectFormat.UseVisualStyleBackColor = true;
+            this.btnSelectFormat.Click += new System.EventHandler(this.btnSelectFormat_Click);
+            // 
+            // lblSelectedFiles
+            // 
+            this.lblSelectedFiles.AutoSize = true;
+            this.lblSelectedFiles.Location = new System.Drawing.Point(14, 88);
+            this.lblSelectedFiles.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSelectedFiles.Name = "lblSelectedFiles";
+            this.lblSelectedFiles.Size = new System.Drawing.Size(68, 17);
+            this.lblSelectedFiles.TabIndex = 8;
+            this.lblSelectedFiles.Text = "未选择文件";
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExtract.Location = new System.Drawing.Point(756, 119);
+            this.btnExtract.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(88, 52);
+            this.btnExtract.TabIndex = 9;
+            this.btnExtract.Text = "解包";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(18, 119);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(731, 30);
+            this.progressBar.TabIndex = 10;
+            this.progressBar.Visible = false;
+            // 
+            // rtbFileNameInfo
+            // 
+            this.rtbFileNameInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbFileNameInfo.Location = new System.Drawing.Point(18, 177);
+            this.rtbFileNameInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rtbFileNameInfo.Name = "rtbFileNameInfo";
+            this.rtbFileNameInfo.Size = new System.Drawing.Size(835, 247);
+            this.rtbFileNameInfo.TabIndex = 11;
+            this.rtbFileNameInfo.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 437);
+            this.Controls.Add(this.rtbFileNameInfo);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.lblSelectedFiles);
+            this.Controls.Add(this.btnSelectFormat);
+            this.Controls.Add(this.lblAvailableFormats);
+            this.Controls.Add(this.btnSelectInputFolder);
+            this.Controls.Add(this.txtInputFolder);
+            this.Controls.Add(this.lblInputFolder);
+            this.Controls.Add(this.btnSelectBmsScript);
+            this.Controls.Add(this.txtBmsScript);
+            this.Controls.Add(this.lblBmsScript);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(581, 242);
+            this.Name = "MainForm";
+            this.Text = "quickbms批量解包工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBmsScript;
+        private System.Windows.Forms.TextBox txtBmsScript;
+        private System.Windows.Forms.Button btnSelectBmsScript;
+        private System.Windows.Forms.Label lblInputFolder;
+        private System.Windows.Forms.TextBox txtInputFolder;
+        private System.Windows.Forms.Button btnSelectInputFolder;
+        private System.Windows.Forms.Label lblAvailableFormats;
+        private System.Windows.Forms.Button btnSelectFormat;
+        private System.Windows.Forms.Label lblSelectedFiles;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.RichTextBox rtbFileNameInfo;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,272 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickBMSBatchExtractor
+{
+    public partial class MainForm : Form
+    {
+        private string bmsScriptPath = "";
+        private string inputFolderPath = "";
+        private List<string> selectedFiles = new List<string>();
+        private List<string> availableFormats = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            txtInputFolder.TextChanged += txtInputFolder_TextChanged;
+        }
+
+        private void btnSelectBmsScript_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "BMS文件(*.bms)|*.bms";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    bmsScriptPath = openFileDialog.FileName;
+                    txtBmsScript.Text = bmsScriptPath;
+                }
+            }
+        }
+
+        private void btnSelectInputFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    inputFolderPath = folderBrowserDialog.SelectedPath;
+                    txtInputFolder.Text = inputFolderPath;
+                    ScanFolderFormats(inputFolderPath);
+                }
+            }
+        }
+
+        private void ScanFolderFormats(string folder)
+        {
+            try
+            {
+                availableFormats.Clear();
+                var extensions = new HashSet<string>();
+
+                foreach (string file in Directory.EnumerateFiles(folder, "*.*", SearchOption.AllDirectories))
+                {
+                    string ext = Path.GetExtension(file).ToLower();
+                    if (!string.IsNullOrEmpty(ext))
+                    {
+                        extensions.Add(ext);
+                    }
+                }
+
+                availableFormats = extensions.OrderBy(x => x).ToList();
+                if (availableFormats.Count > 0)
+                {
+                    lblAvailableFormats.Text = $"检测到格式: {string.Join(", ", availableFormats)}";
+                    AppendToRichTextBox($"文件夹扫描完成，找到以下格式: {string.Join(", ", availableFormats)}\n");
+                }
+                else
+                {
+                    lblAvailableFormats.Text = "文件夹中没有文件";
+                    AppendToRichTextBox("文件夹中没有找到任何文件\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendToRichTextBox($"扫描文件夹时出错: {ex.Message}\n");
+            }
+        }
+
+        private void btnSelectFormat_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputFolderPath))
+            {
+                AppendToRichTextBox("请先选择文件夹\n");
+                return;
+            }
+
+            if (availableFormats.Count == 0)
+            {
+                AppendToRichTextBox("文件夹中没有可用的文件格式\n");
+                return;
+            }
+
+            var fileTypes = new List<string>();
+            foreach (string format in availableFormats)
+            {
+                string extName = format.Substring(1).ToUpper() + " files";
+                fileTypes.Add($"{extName}|*{format}");
+            }
+            fileTypes.Add("所有文件|*.*");
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = inputFolderPath;
+                openFileDialog.Multiselect = true;
+                openFileDialog.Filter = string.Join("|", fileTypes);
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFiles = openFileDialog.FileNames.ToList();
+                    UpdateSelectedFilesLabel();
+                    AppendToRichTextBox($"已选择 {selectedFiles.Count} 个 {Path.GetExtension(selectedFiles.First())} 文件\n");
+                }
+            }
+        }
+
+        private void UpdateSelectedFilesLabel()
+        {
+            if (selectedFiles.Count > 0)
+            {
+                string ext = Path.GetExtension(selectedFiles[0]);
+                lblSelectedFiles.Text = $"已选择 {selectedFiles.Count} 个 {ext} 文件";
+            }
+            else
+            {
+                lblSelectedFiles.Text = "未选择文件";
+            }
+        }
+
+        private string GetQuickBMSExecutable(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            return fileInfo.Length > 3L * 1024 * 1024 * 1024 ? "quickbms_4gb_files.exe" : "quickbms.exe";
+        }
+
+        private void AppendToRichTextBox(string text)
+        {
+            if (this.rtbFileNameInfo.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate {
+                    rtbFileNameInfo.AppendText(text);
+                });
+            }
+            else
+            {
+                rtbFileNameInfo.AppendText(text);
+            }
+        }
+
+        private async Task ExtractFilesAsync(string bmsScript, List<string> files, IProgress<int> progress)
+        {
+            await Task.Run(() =>
+            {
+                int totalFiles = files.Count;
+                int currentFiles = 0;
+
+                foreach (string filePath in files)
+                {
+                    try
+                    {
+                        AppendToRichTextBox($"正在处理: {Path.GetFileName(filePath)}\n");
+
+                        string quickbmsExecutable = GetQuickBMSExecutable(filePath);
+                        string? directoryName = Path.GetDirectoryName(filePath);
+                        if (directoryName == null)
+                        {
+                            AppendToRichTextBox($"无法获取文件夹路径: {filePath}\n");
+                            continue;
+                        }
+
+                        string outputFolder = Path.Combine(
+                            directoryName,
+                            Path.GetFileNameWithoutExtension(filePath));
+
+                        if (!Directory.Exists(outputFolder))
+                        {
+                            Directory.CreateDirectory(outputFolder);
+                        }
+
+                        string fileExtension = "*" + Path.GetExtension(filePath);
+                        ProcessStartInfo startInfo = new ProcessStartInfo
+                        {
+                            FileName = quickbmsExecutable,
+                            Arguments = $"-o -F \"{fileExtension}\" \"{bmsScript}\" \"{filePath}\" \"{outputFolder}\"",
+                            UseShellExecute = false,
+                            CreateNoWindow = true
+                        };
+
+                        using (Process? process = Process.Start(startInfo))
+                        {
+                            if (process == null)
+                            {
+                                AppendToRichTextBox($"启动进程失败，处理文件 {filePath} 时出错\n");
+                                continue;
+                            }
+                            process.WaitForExit();
+                        }
+
+                        currentFiles++;
+                        int percentage = (int)((double)currentFiles / totalFiles * 100);
+                        progress.Report(percentage);
+
+                        AppendToRichTextBox($"完成: {Path.GetFileName(filePath)}\n");
+                    }
+                    catch (Exception ex)
+                    {
+                        AppendToRichTextBox($"处理文件 {filePath} 时出错: {ex.Message}\n");
+                    }
+                }
+            });
+        }
+
+        private async void btnExtract_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(bmsScriptPath))
+            {
+                AppendToRichTextBox("请选择BMS脚本\n");
+                return;
+            }
+
+            if (selectedFiles.Count == 0)
+            {
+                AppendToRichTextBox("请选择要解包的文件\n");
+                return;
+            }
+
+            btnExtract.Enabled = false;
+            progressBar.Value = 0;
+            progressBar.Visible = true;
+            AppendToRichTextBox($"开始解包 {selectedFiles.Count} 个文件...\n");
+
+            try
+            {
+                var progress = new Progress<int>(percent =>
+                {
+                    progressBar.Value = percent;
+                });
+
+                await ExtractFilesAsync(bmsScriptPath, selectedFiles, progress);
+
+                AppendToRichTextBox("解包完成!\n");
+            }
+            catch (Exception ex)
+            {
+                AppendToRichTextBox($"解包过程中出错: {ex.Message}\n");
+            }
+            finally
+            {
+                btnExtract.Enabled = true;
+            }
+        }
+
+        private void txtInputFolder_TextChanged(object? sender, EventArgs e)
+        {
+            string inputPath = txtInputFolder.Text;
+            if (!string.IsNullOrEmpty(inputPath) && Directory.Exists(inputPath))
+            {
+                inputFolderPath = inputPath;
+                ScanFolderFormats(inputPath);
+            }
+            else if (!string.IsNullOrEmpty(inputPath) && !Directory.Exists(inputPath))
+            {
+                AppendToRichTextBox("输入的文件夹路径不存在，请检查后重新输入或选择。\n");
+                txtInputFolder.Text = "";
+            }
+        }
+    }
+}
